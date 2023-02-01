1859d934368de001dcc3423b4462e5cc6c3925b0
Nerea <nereaalonso@ceroca.cat>
2023-02-01 19:32:09 +0100

    Leyouts + Java Part

--- app/src/main/java/com/example/firebase_mvvm/MainActivity.java
+++ app/src/main/java/com/example/firebase_mvvm/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.firebase_mvvm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mEtNom, mEtIngredients, mEtPreu;
+    private Button mBtnAfegir, mBtnActualitzar, mBtnEsborrar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        InicialitzarComponents();
+    }
+
+    private void InicialitzarComponents(){
+
+        mEtNom = findViewById(R.id.ET_Nom);
+        mEtIngredients = findViewById(R.id.ET_Ingredients);
+        mEtPreu = findViewById(R.id.ET_Preu);
+        mBtnAfegir = findViewById(R.id.BTN_Afegir);
+        mBtnActualitzar = findViewById(R.id.BTN_Actualitzar);
+        mBtnEsborrar = findViewById(R.id.BTN_Esborrar);
     }
 }
