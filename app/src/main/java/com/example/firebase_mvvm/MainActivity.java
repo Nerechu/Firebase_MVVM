7bdbe2301e0f2c044b883f94685221b41937e1ba
Nerea <nereaalonso@ceroca.cat>
2023-02-01 20:24:35 +0100

    Commit 3; Conectat amb Firebase i listeners preparats.

--- app/src/main/java/com/example/firebase_mvvm/MainActivity.java
+++ app/src/main/java/com/example/firebase_mvvm/MainActivity.java
@@ -3,13 +3,20 @@ package com.example.firebase_mvvm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText mEtNom, mEtIngredients, mEtPreu;
     private Button mBtnAfegir, mBtnActualitzar, mBtnEsborrar;
+
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mReference;
 
 
     @Override
@@ -18,6 +25,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         InicialitzarComponents();
+        InicialitzarListeners();
+    }
+
+    private  void InicialitzarListeners(){
+        mBtnAfegir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AfegirPizza();
+                
+            }
+        });
+
+        mBtnActualitzar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+                ActualitzarPizza();
+
+            }
+        });
+
+        mBtnEsborrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+                EsborrarPizza();
+                
+            }
+        });
+    }
+
+    private void EsborrarPizza() {
+    }
+
+    private void ActualitzarPizza() {
+    }
+
+    private void AfegirPizza() {
     }
 
     private void InicialitzarComponents(){
@@ -28,5 +74,8 @@ public class MainActivity extends AppCompatActivity {
         mBtnAfegir = findViewById(R.id.BTN_Afegir);
         mBtnActualitzar = findViewById(R.id.BTN_Actualitzar);
         mBtnEsborrar = findViewById(R.id.BTN_Esborrar);
+
+        mDatabase = FirebaseDatabase.getInstance();
+        mReference = mDatabase.getReference();
     }
 }
